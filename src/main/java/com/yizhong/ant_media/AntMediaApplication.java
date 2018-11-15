6ad35cf12baf856f1f16b7006c2397b053954744
kano <avk458@gmail.com>
2018-11-15 15:45:37 +0800

    Initial commit

---
+++ src/main/java/com/yizhong/ant_media/AntMediaApplication.java
@@ -0,0 +1,12 @@
+package com.yizhong.ant_media;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntMediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AntMediaApplication.class, args);
+    }
+}
